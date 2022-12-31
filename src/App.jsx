244812ae1431fa0162fd5f5e6e2dd3369a5f543a
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2022-12-31 19:39:00 +0500

    Git Init

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+function App() {
+  return (
+    <AppContainer>
+      {/* <NikeCard /> */}
+    </AppContainer>
+  );
+};
+
+export default App;
